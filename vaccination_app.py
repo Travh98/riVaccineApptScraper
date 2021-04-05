b34a3814888421b711814f4816c83297450d37f6
glanowy <58753106+glanowy@users.noreply.github.com>
2021-04-05 12:57:21 -0400

    Update vaccination_app.py
    gui creates labels for available locations and displays a button to book an appointment at each one. Includes location and date information for additional scrapping

--- vaccination_app.py
+++ vaccination_app.py
@@ -11,120 +11,74 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
-import vaccine_appt2 as vac
+import vaccine_scrapper as vac
 
 titleList = []
 apptNumList = []
     
 class COVID(tk.Tk):
     
-    def Scrape(self):
-            vac.scrapevaccineappt(self.zipcode.get())
+    def make_an_appointment(self, location, date):            #New window to enter personal information
+        newWin = tk.Toplevel(root)
+        newWin.title("Enter information here")
+        newWin.geometry("200x200")
+        self.name = tk.StringVar()
+        ttk.Label(newWin, text = "Enter Name: ").grid(column = 0, row = 0, pady = 10)
+        ttk.Entry(newWin, width = 15, textvariable = self.name).grid(column = 1, row = 0, pady = 10)
+        print(location)
+        return
     
-    # def Scrape(self):
-    #     # print("get list of appointments close to: " + str(self.zipcode.get()))
-    #     for i in range(3):
-    #         i = str(i + 1)
-    #         print("Page: ", i)
-    #         govWebsiteSchedule = 'https://www.vaccinateri.org/clinic/search?location=' + str(self.zipcode.get()) + '&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search&page=' + i + '#search_results'
-    #         result = requests.get(govWebsiteSchedule)
-    #         # If code == 200, page is accessable
-    #         print("STATUS CODE: ", result.status_code)
-        
-    #         # Page content of the website
-    #         src = result.content
-        
-    #         # Beautiful Soup object
-    #         soup = BeautifulSoup(src, 'lxml')
-        
-                
-    #         pTitle = soup.find_all("p", "text-xl font-black")
-            
-    #         for x in range(len(pTitle)):
-    #             title = str(pTitle[x].text).replace('\n', '')
-    #             title = title.lstrip()
-    #             title = title.rstrip()
-    #             if self.date_entry.get() == 'MM/DD/YYYY':
-    #                 titleList.append(title)
-    #             elif self.date_entry.get() in title:
-    #                 titleList.append(title.replace(' on ' + str(self.date_entry.get()), ''))
-    #         print(titleList)
-        
-    #         paragraphs = soup.find_all("p")     #Attempt: Search just for appointments available on the date
-    #         title = str(pTitle[x].text).replace('\n', '')
-    #         title = title.lstrip()
-    #         title = title.rstrip()
-            
-    #         if self.date_entry.get() == 'MM/DD/YYYY':           #Get all available places 
-    #             for paragraph in paragraphs:
-    #                 if "Appointments Available" in paragraph.text:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0: 
-    #                         apptNumList.append(apptNumber)
-    #                 # Spanish
-    #                 if "Citas disponibles" in paragraph.text:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0:
-    #                         apptNumList.append(apptNumber)
-    #                 # Portuguese
-    #                 if "Agendamentos disponíveis" in paragraph.text:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0:
-    #                         apptNumList.append(apptNumber)
-    #         else:                                               #Get available places with respect to date
-    #             for paragraph in paragraphs:
-    #                 if "Appointments Available" in paragraph.text and self.date_entry.get() in title:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0: 
-    #                         apptNumList.append(apptNumber)
-    #                 # Spanish
-    #                 if "Citas disponibles" in paragraph.text and self.date_entry.get() in title:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0:
-    #                         apptNumList.append(apptNumber)
-    #                 # Portuguese
-    #                 if "Agendamentos disponíveis" in paragraph.text and self.date_entry.get() in title:
-    #                     apptNumber = int(paragraph.text.replace(paragraph.strong.text, ''))
-    #                     if apptNumber != 0:
-    #                         apptNumList.append(apptNumber)
-    #         print(apptNumList)
-    #     return
+    def Scrape(self):                                    #Method to isolate and display available appointments with respect to date
+        if self.date_entry.get() == "MM/DD/YYYY":
+            vaccdata = vac.scrapevaccineappt(self.zipcode.get())
+            vac_available = vac.displayavailableappts(vaccdata).to_string(index=False)
+            print(vac_available)
+            vac_data = vac.displayavailableappts(vaccdata)
+            i = 2
+            for index, row in vac_data.iterrows():
+                sitei = ttk.Label(self, text = str(row["Location"])).grid(column = 0, row = i)
+                appi = ttk.Label(self, text = str(row["Appointments"])).grid(column = 1, row = i)
+                datei = ttk.Label(self, text = str(row["Date"])).grid(column = 2, row = i)
+                buttoni = ttk.Button(self, text = "Book Here!", command = lambda : self.make_an_appointment(row["Location"], row["Date"])).grid(column = 3, row = i)
+                i += 1 
+        else:
+            vaccination_data = vac.scrapevaccineappt(self.zipcode.get())
+            vaccdata = vac.apptsmatchingdate(vaccination_data, self.date_entry.get())
+            vac_available = vac.displayavailableappts(vaccdata).to_string(index=False)
+            print(vac_available)
+            vac_data = vac.displayavailableappts(vaccdata)
+            i = 2
+            for index, row in vac_data.iterrows():
+                sitei = ttk.Label(self, text = str(row["Location"])).grid(column = 0, row = i)
+                appi = ttk.Label(self, text = str(row["Appointments"])).grid(column = 1, row = i)
+                datei = ttk.Label(self, text = str(row["Date"])).grid(column = 2, row = i)
+                buttoni = ttk.Button(self, text = "Book Here!", command = lambda : self.make_an_appointment(row["Location"], row["Date"])).grid(column = 3, row = i)
+                i += 1     
     
     def __init__(self):
         super().__init__()
         self.title("COVID-19 Vaccine Appointments")
-        self.geometry("400x180")
+        self.geometry("650x180")
         
         self.zipcode = tk.StringVar()
         self.date_entry = tk.StringVar()
         
-        zip_c = ttk.Button(self, text = "Enter Zip Code: ", command = self.Scrape)
+        scrape = ttk.Button(self, text = "Find Appointments", command = self.Scrape).grid(column = 4, row = 0, pady = 10)
+        
+        zip_c = ttk.Label(self, text = "Enter Zip Code: ") #lambda : vac.scrapevaccineappt(self.zipcode.get())   
         date_label = ttk.Label(self, text = "Enter Date: ")
         zip_entry = ttk.Entry(self, width = 8, textvariable = self.zipcode)
         date_entry = ttk.Entry(self, width = 14, textvariable = self.date_entry)
         date_entry.insert(tk.END, 'MM/DD/YYYY')
         
-        date_entry.grid(column = 3, row = 0, sticky = "N")
+        date_entry.grid(column = 3, row = 0)
         date_label.grid(column = 2, row = 0)
-        zip_c.grid(column = 0, row = 0, sticky = "N")
+        zip_c.grid(column = 0, row = 0)
         zip_entry.grid(column = 1, row = 0, sticky = "W")
         
-        no_appointments = ttk.Label(self, text = "Available Appointments")
-        no_appointments.grid(column = 1, row = 1)
-        
-        #for entry in titleList:         #Find out how to update placeholder list
-        site1 = ttk.Label(self, text = "Placeholder Site 1")
-        site2 = ttk.Label(self, text = "Placeholder Site 2")
-        site3 = ttk.Label(self, text = "Placeholder Site 3")
-        
-        site1_app = ttk.Label(self, text = "4").grid(column = 1, row = 2)
-        site2_app = ttk.Label(self, text = "2").grid(column = 1, row = 3)
-        site3_app = ttk.Label(self, text = "0").grid(column = 1, row = 4)
-        
-        site1.grid(column = 0, row = 2)
-        site2.grid(column = 0, row = 3)
-        site3.grid(column = 0, row = 4)
-    
+        loc_appointments = ttk.Label(self, text = "Location").grid(column = 0, row = 1)
+        no_appointments = ttk.Label(self, text = "Available Appointments").grid(column = 1, row = 1)
+        date_appointments = ttk.Label(self, text = "Date").grid(column = 2, row = 1)    
         
 root = COVID()
 root.mainloop()
